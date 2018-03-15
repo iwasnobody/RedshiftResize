cded2bc626bef728af5dbb75312a201012705cdb
Ivan Yu <ivanyu1987@gmail.com>
2018-03-15 14:07:53 -0400

    Create TriggerStepFunc.py

---
+++ TriggerStepFunc.py
@@ -0,0 +1,10 @@
+import boto3
+import os
+
+client = boto3.client('stepfunctions',region_name='ap-northeast-1')
+
+def lambda_handler(event, context):
+    response = client.start_execution(
+        stateMachineArn=os.environ['StepFuncArn'],
+        input="{\"Action\" : \"stopdms\"}"
+    )
