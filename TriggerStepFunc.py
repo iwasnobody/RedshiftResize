6e892b6bca197b25cc299deaf2833d7dc248d729
Ivan Yu <ivanyu1987@gmail.com>
2018-03-16 05:08:37 -0400

    Update TriggerStepFunc.py

--- TriggerStepFunc.py
+++ TriggerStepFunc.py
@@ -6,5 +6,5 @@ client = boto3.client('stepfunctions',region_name='ap-northeast-1')
 def lambda_handler(event, context):
     response = client.start_execution(
         stateMachineArn=os.environ['StepFuncArn'],
-        input="{\"Action\" : \"stopdms\"}"
+        input="{\"Action\" : \"stopdms\",\"Error-info\" : \"None\"}"
     )
